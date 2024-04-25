31ae923b24a0e69ffbef29705bfd8effb626be67
LAPTOP-AVTQTJ12\hiros <hirosima7210506@gmail.com>
2024-04-26 01:30:21 +0900

    ブログシステムサイト作成

---
+++ BlogSystem/Controllers/BlogsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogSystem.Models;
+
+namespace BlogSystem.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private BlogContext db = new BlogContext();
+
+        // GET: Blogs
+        public ActionResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+        // GET: Blogs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Blogs/Create
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,postDate,Content")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+
+        // GET: Blogs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Blogs/Edit/5
+        // 過多ポスティング攻撃を防止するには、バインド先とする特定のプロパティを有効にしてください。
+        // 詳細については、https://go.microsoft.com/fwlink/?LinkId=317598 を参照してください。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,postDate,Content")] Blog blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
+        }
+
+        // GET: Blogs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Blog blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        // POST: Blogs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blog blog = db.Blogs.Find(id);
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- BlogSystem/Models/Blog.cs
+++ BlogSystem/Models/Blog.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlogSystem.Models
 {
     public class Blog
     {
         public int Id { get; set; }
+        [Display(Name ="タイトル")]
         public string Title { get; set; }
+        [Display(Name = "投稿日時")]
         public DateTime postDate { get; set; }
+        [Display(Name = "投稿内容")]
         public string Content { get; set; }   
     }
 }
